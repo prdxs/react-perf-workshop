3cb8d5558d6762c0dba5750af6fed2ae15540c0f
Ruben Fernandez-Butron <ruben.fernandez@missguided.com>
2020-11-12 19:09:42 +0000

    EX1.3: Use useCallback. Now! But, what about the counter?

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import Button from "./components/Button";
 import "./App.css";
 
 function App() {
   console.log("App: rendering");
-  const [count, setCout] = React.useState(0);
+  const [count, setCout] = useState(0);
+
+  const handleClick = useCallback(() => setCout(count + 1), []);
 
   return (
     <div className="App">
       <p>{count}</p>
-      <Button onClick={() => setCout(count+1)}>+1</Button>
+      <Button onClick={handleClick}>+1</Button>
     </div>
   );
 }
