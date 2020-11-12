0840a375544044fa4eb7d78d2269f599f1f31d2f
Ruben Fernandez-Butron <ruben.fernandez@missguided.com>
2020-11-12 19:44:21 +0000

    EX2.2: Sorted! But what if we want our style to be dependant on a theme?

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import React, { useCallback, useState } from "react";
 import Button from "./components/Button";
 import "./App.css";
 
+const buttonStyle = {
+  borderRadius: 0,
+  border: "1px solid black",
+  padding: "0.5rem 1rem",
+  boxShadow: "5px 10px",
+  outline: "none",
+  cursor: "pointer",
+};
+
 function App() {
   console.log("App: rendering");
   const [count, setCount] = useState(0);
 
   const handleClick = useCallback(() => setCount((count) => count + 1), []);
-
-  const buttonStyle = {
-    borderRadius: 0,
-    border: "1px solid black",
-    padding: "0.5rem 1rem",
-    boxShadow: "5px 10px",
-    outline: "none",
-    cursor: "pointer",
-  };
 
   return (
     <div className="App">
