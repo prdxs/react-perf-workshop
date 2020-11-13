4b9b430efd9af29c82b18891b28a55660cee1c7f
Ruben Fernandez-Butron <ruben.fernandez@missguided.com>
2020-11-13 08:18:11 +0000

    EX2.3: What now? Which button is re-rendering? What's happening when we change the theme?

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,49 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useMemo } from "react";
 import Button from "./components/Button";
 import "./App.css";
 
-const buttonStyle = {
-  borderRadius: 0,
-  border: "1px solid black",
-  padding: "0.5rem 1rem",
-  boxShadow: "5px 10px",
-  outline: "none",
-  cursor: "pointer",
-};
+const themes = [{ shadowColor: "black" }, { shadowColor: "red" }];
 
 function App() {
   console.log("App: rendering");
   const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState(themes[0]);
 
   const handleClick = useCallback(() => setCount((count) => count + 1), []);
+  const changeTheme = useCallback(() => {
+    if (theme === themes[0]) {
+      setTheme(themes[1]);
+    } else {
+      setTheme(themes[0]);
+    }
+  }, [theme]);
+
+  const buttonStyle = useMemo(
+    () => ({
+      borderRadius: 0,
+      border: `1px solid black`,
+      padding: "0.5rem 1rem",
+      boxShadow: `5px 10px ${theme.shadowColor}`,
+      outline: "none",
+      cursor: "pointer",
+    }),
+    []
+  );
 
   return (
-    <div className="App">
-      <p>{count}</p>
-      <Button style={buttonStyle} onClick={handleClick}>
-        +1
-      </Button>
-    </div>
+    <>
+      <div className="App-controls">
+        <Button onClick={changeTheme}>
+          change theme
+        </Button>
+      </div>
+      <div className="App">
+        <p>{count}</p>
+        <Button style={buttonStyle} onClick={handleClick}>
+          +1
+        </Button>
+      </div>
+    </>
   );
 }
 
